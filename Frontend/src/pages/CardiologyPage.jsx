5a63e7fe119554bf101bc9b2c991dd14b9d76472
Anakha S <anakhas771@gmail.com>
2025-11-22 18:48:09 +0530

    Set API

--- Frontend/src/pages/CardiologyPage.jsx
+++ Frontend/src/pages/CardiologyPage.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+/* eslint-disable no-unused-vars */
 import { motion } from "framer-motion";
 import DoctorCard from "../components/DoctorCard";
 import PageSection from "../components/PageSection";
