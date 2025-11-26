2c798f8df9c23cc76066e0bb38ca6903a006b56e
Anakha S <anakhas771@gmail.com>
2025-11-26 23:47:03 +0530

    updated email verification

--- Backend/accounts/models.py
+++ Backend/accounts/models.py
@@ -33,6 +33,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     is_doctor = models.BooleanField(default=False)
     reset_token = models.CharField(max_length=64, blank=True, null=True)
     date_joined = models.DateTimeField(auto_now_add=True)
+    is_verified = models.BooleanField(default=False)
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
