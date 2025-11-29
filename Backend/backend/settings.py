321092959abd1ea0240a4f803a85f98e685a6b24
Anakha S <anakhas771@gmail.com>
2025-11-29 20:56:09 +0530

    updated email

--- Backend/backend/settings.py
+++ Backend/backend/settings.py
@@ -78,12 +78,12 @@ WSGI_APPLICATION = "backend.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": config("DB_NAME"),
-        "USER": config("DB_USER"),
-        "PASSWORD": config("DB_PASSWORD"),
-        "HOST": config("DB_HOST"),
+        "NAME": config("DB_NAME", default="Hopedb"),
+        "USER": config("DB_USER", default="postgres"),
+        "PASSWORD": config("DB_PASSWORD", default="1234"),
+        "HOST": config("DB_HOST", default="127.0.0.1"),
         "PORT": config("DB_PORT", default="5432"),
-    }
+            }
 }
 
 # ---------------------------------------------------------
