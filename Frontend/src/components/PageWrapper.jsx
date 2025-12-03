7d478e317facfaf1e969bbabfa509cfc0e553f19
Anakha S <anakhas771@gmail.com>
2025-12-03 21:07:26 +0530

    payment fixed

--- Frontend/src/components/PageWrapper.jsx
+++ Frontend/src/components/PageWrapper.jsx
@@ -6,7 +6,6 @@ const PageWrapper = ({ children }) => (
   <motion.div
     initial={{ opacity: 0, y: 30 }}
     animate={{ opacity: 1, y: 0 }}
-    exit={{ opacity: 0, y: 30 }}
     transition={{ duration: 0.5 }}
   >
     {children}
