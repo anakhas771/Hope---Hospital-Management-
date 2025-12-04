1036c19ae4db3dfee2c4b6a82f62040aaf1b7587
Anakha S <anakhas771@gmail.com>
2025-12-04 22:30:12 +0530

    Fix admin login for email authentication

--- Backend/accounts/backends.py
+++ Backend/accounts/backends.py
@@ -1,19 +1,17 @@
-from django.contrib.auth.backends import ModelBackend
+from django.contrib.auth.backends import BaseBackend
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
-class EmailBackend(ModelBackend):
-    """
-    Authenticate using email instead of username
-    """
+class EmailBackend(BaseBackend):
     def authenticate(self, request, username=None, password=None, **kwargs):
         try:
             user = User.objects.get(email=username)
-            if user.check_password(password):
-                return user
         except User.DoesNotExist:
             return None
+        if user.check_password(password) and user.is_active:
+            return user
+        return None
 
     def get_user(self, user_id):
         try:
