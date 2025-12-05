209b54e516bd30448e218e869aa4bb2c9f4a001f
Anakha S <anakhas771@gmail.com>
2025-12-05 10:57:36 +0530

    admin dashboard fixed

--- Backend/accounts/admin.py
+++ Backend/accounts/admin.py
@@ -1,67 +1,33 @@
-# accounts/admin.py
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from django import forms
-from django.contrib.auth.forms import ReadOnlyPasswordHashField
-
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import User, Department, Doctor, Appointment
 
 
-# -------------------- CUSTOM FORMS --------------------
-
-class CustomUserCreationForm(forms.ModelForm):
-    """Form used in the admin to create new users"""
-    password1 = forms.CharField(label="Password", widget=forms.PasswordInput)
-    password2 = forms.CharField(label="Confirm Password", widget=forms.PasswordInput)
-
+class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ("email", "first_name", "last_name", "is_staff", "is_active", "is_doctor", "is_patient")
-
-    def clean_password2(self):
-        p1 = self.cleaned_data.get("password1")
-        p2 = self.cleaned_data.get("password2")
-        if p1 and p2 and p1 != p2:
-            raise forms.ValidationError("Passwords do not match")
-        return p2
-
-    def save(self, commit=True):
-        user = super().save(commit=False)
-        user.set_password(self.cleaned_data["password1"])
-        if commit:
-            user.save()
-        return user
+        fields = ("email", "first_name", "last_name")
 
 
-class CustomUserChangeForm(forms.ModelForm):
-    """Form used in admin to update existing users"""
-    password = ReadOnlyPasswordHashField()
-
+class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = User
         fields = (
             "email",
-            "password",
             "first_name",
             "last_name",
             "is_active",
             "is_staff",
             "is_superuser",
-            "is_doctor",
             "is_patient",
-            "groups",
-            "user_permissions",
+            "is_doctor",
         )
 
-    def clean_password(self):
-        return self.initial["password"]
-
-
-# -------------------- CUSTOM USER ADMIN --------------------
 
 class UserAdmin(BaseUserAdmin):
-    form = CustomUserChangeForm
     add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
 
     list_display = (
         "email",
@@ -73,17 +39,24 @@ class UserAdmin(BaseUserAdmin):
         "is_doctor",
         "is_patient",
     )
+
     search_fields = ("email", "first_name", "last_name")
     ordering = ("email",)
-    list_filter = ("is_staff", "is_superuser", "is_active", "is_doctor", "is_patient")
 
     fieldsets = (
         (None, {"fields": ("email", "password")}),
         ("Personal Info", {"fields": ("first_name", "last_name")}),
-        (
-            "Permissions",
-            {"fields": ("is_active", "is_staff", "is_superuser", "is_doctor", "is_patient", "groups", "user_permissions")},
-        ),
+        ("Permissions", {
+            "fields": (
+                "is_active",
+                "is_staff",
+                "is_superuser",
+                "is_doctor",
+                "is_patient",
+                "groups",
+                "user_permissions",
+            )
+        }),
         ("Important Dates", {"fields": ("last_login", "date_joined")}),
     )
 
@@ -96,16 +69,14 @@ class UserAdmin(BaseUserAdmin):
                 "last_name",
                 "password1",
                 "password2",
-                "is_active",
                 "is_staff",
+                "is_active",
                 "is_doctor",
                 "is_patient",
             ),
         }),
     )
 
-
-# -------------------- REGISTER MODELS --------------------
 
 admin.site.register(User, UserAdmin)
 admin.site.register(Department)
--- Backend/accounts/backends.py
+++ Backend/accounts/backends.py
@@ -1,20 +1,26 @@
-from django.contrib.auth.backends import BaseBackend
+from django.contrib.auth.backends import ModelBackend
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
-class EmailBackend(BaseBackend):
-    def authenticate(self, request, username=None, password=None, **kwargs):
-        try:
-            user = User.objects.get(email=username)
-        except User.DoesNotExist:
-            return None
-        if user.check_password(password) and user.is_active:
-            return user
-        return None
 
-    def get_user(self, user_id):
+class EmailBackend(ModelBackend):
+    """
+    Custom authentication backend that allows users to log in using their email instead of username.
+    """
+
+    def authenticate(self, request, username=None, password=None, **kwargs):
+        email = username or kwargs.get("email")
+
+        if email is None or password is None:
+            return None
+
         try:
-            return User.objects.get(pk=user_id)
+            user = User.objects.get(email=email)
         except User.DoesNotExist:
             return None
+
+        if user.check_password(password):
+            return user
+
+        return None
